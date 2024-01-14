1beeda18d4e2e2fd7fa4e6351ecc415df751476a
Phuong Huynh <phuynh@axon.com>
2024-01-14 21:40:24 +0700

    init commit

---
+++ lesson_1/VariableLearning.js
@@ -0,0 +1,20 @@
+// Naming convention: [v]camelCase - variable, [x]snake_case, [x]PascalCase
+let myName = 'Tui'
+myName = 18
+console.log(myName);
+
+// Constant | ALL_WORDS_ARE_UPPER_CASE
+const PI_NUMBER = 3.14
+//PI_NUMBER = 4
+
+//1.7976931348623157e+308 = 1797693134862315700000...0000 22 chữ số 0
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(Infinity > 1.7976931348623157e+309)
+
+let amIRight = true
+console.log(typeof amIRight);
+console.log(typeof myName);
+console.log(1.7976931348623157e+309);
+
+//Push into github repo
